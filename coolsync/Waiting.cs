8453498a1d0437a4573a573279666aa9a8dad703
0050062383 <0050062383@LAB1_TERMINAL15>
2016-10-03 20:04:20 -0300

    Add project files.

---
+++ coolsync/Waiting.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace coolsync
+{
+    public class Waiting
+    {
+        private CancellationTokenSource ReferenciarNoCancellationToken { get; set; }
+        public CancellationToken Cancel { get; set; }
+
+        public Waiting()
+        {
+            ReferenciarNoCancellationToken = new CancellationTokenSource();
+            Cancel = ReferenciarNoCancellationToken.Token;
+        }
+        private CancellationToken ReferenciarNoTask()
+        {
+            try
+            {
+                CancellationToken ct = ReferenciarNoCancellationToken.Token;
+                return ct;
+            }
+            catch (Exception erro)
+            {
+
+                throw new Exception(erro.Message);
+            }
+
+        }
+
+        public void CancelTask(Form form)
+        {
+            try
+            {
+                ReferenciarNoCancellationToken.Cancel();
+
+                if (this.Cancel.IsCancellationRequested)
+                    form?.Close();
+            }
+            catch (Exception erro)
+            {
+
+                throw new Exception(erro.Message);
+            }
+
+        }
+        public void Start(Action action)
+        {
+            try
+            {
+                Control.CheckForIllegalCrossThreadCalls = false;
+                new Task(action, Cancel).Start();
+            }
+            catch (Exception erro)
+            {
+
+                throw new Exception(erro.Message);
+            }
+
+        }
+    }
+}
